b42b34e9927f5d922868c02bb16895a4fc662b7f
Kelley, Alexander W <kelleyaw@iu.edu>
2020-05-08 10:40:30 -0400

    Create Quiz.js
    Since I have a lot of time today I can take the big functions CreateQuestion, displayNext, and displayScore. You can work on createButtons(index) and choose(). They should be relativley simple and if you finish those functions quickly and have time you can work on the Click Handlers or QoL animations :+1:

---
+++ Quiz.js
@@ -0,0 +1,32 @@
+// Creates and returns the div that contains the questions and
+// the answer selections
+function createQuestion(index) {
+
+}
+
+// Creates a list of the answer choices (as radio inputs)
+function createButtons(index) {
+
+}
+
+// Reads the user selection and pushes the value to an array
+function choose() {
+
+}
+
+// Displays next requested element
+function displayNext() {
+
+}
+
+// Computes score and returns element to be displayed
+// (probably paragraph element)
+function displayScore() {
+  
+}
+
+// Various Click Handelers
+
+// The Question List
+
+// Small Animations (QoL pieces)
