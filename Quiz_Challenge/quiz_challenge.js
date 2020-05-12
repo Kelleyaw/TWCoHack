e61af4891d11cc69a90f7743e855740ec1151501
Kelley, Alexander W <kelleyaw@iu.edu>
2020-05-12 10:00:51 -0400

    Revert "Finished Challenge Quiz"
    This reverts commit 05f546d90019e067ab88e92f0a009398ca14ac0d.

--- Quiz_Challenge/quiz_challenge.js
+++ Quiz_Challenge/quiz_challenge.js
@@ -30,6 +30,11 @@
       "Yes; FindHelp",
       "Yes; Resource Roundup"],
       correctAnswer: "No; Indiana211"
+    },
+    {
+        question: "",
+        choices: [],
+        correctAnswer: ""
     }
   ];
 
